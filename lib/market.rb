097d4ddc30d88c6a6d1957f560dc3a620c08a8f1
normanrs <normanrs@live.com>
2018-09-12 15:02:06 -0600

    working soted items

--- lib/market.rb
+++ lib/market.rb
@@ -17,5 +17,22 @@ class Market
 			vendor.name
 		end
 	end
-	
+
+	def vendors_that_sell(item_in)
+		found_vendors = []
+		@vendors.each do |vendor|
+      found_vendors << vendor if (vendor.inventory.keys).include? item_in
+    end
+		found_vendors
+	end
+
+	def sorted_item_list
+		all_items = []
+		unique_items = []
+		vendors.each do |vendor|
+			all_items << vendor.inventory.keys
+		end
+		unique_items = (all_items.flatten).uniq
+		unique_items.sort
+	end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -38,6 +38,7 @@ class MarketTest <  Minitest::Test
     market.add_vendor(vendor_3)
     actual = market.vendors.count
     expected = 3
+    assert_equal expected, actual
   end
 
   def test_it_can_add_vendors_with_stock
@@ -53,13 +54,45 @@ class MarketTest <  Minitest::Test
     market.add_vendor(vendor_1)
     market.add_vendor(vendor_2)
     market.add_vendor(vendor_3)
-    actual = market.vendor_names
-    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    actual = market.vendors
+    expected = [vendor_1, vendor_2, vendor_3]
+    assert_equal expected, actual
   end
 
-  # pry(main)> market.vendors_that_sell("Peaches")
-  # #=> [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe134910650...>]
-  #
-  # pry(main)> market.vendors_that_sell("Banana Nice Cream")
-  # #=> [#<Vendor:0x00007fe1349bed40...>]
+  def test_it_can_find_vendors_that_sell_item
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    assert_equal [vendor_1, vendor_3], market.vendors_that_sell("Peaches")
+    assert_equal [vendor_2], market.vendors_that_sell("Banana Nice Cream")
+  end
+
+  def test_it_can_return_sorted_item_list
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+    actual = market.sorted_item_list
+    expected = ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"]
+    assert_equal expected, actual
+  end
+
+
 end
